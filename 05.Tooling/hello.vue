1fbe9e4deebe98296ffdf2d306df60983b796648
Wilder Lizama Garcia <wilderlizama@gmail.com>
2018-09-19 11:40:39 -0500

    Tooling

---
+++ 05.Tooling/hello.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>{{ hel }} World!</p>
+</template>
+
+<script>
+    module.exports = {
+        data : function () {
+            return {
+                hel : "Hello"
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center
+    }
+</style>
