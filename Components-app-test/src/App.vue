70583150ab1549d90fd0afc6af14b9105dc8ab92
Wilder Lizama Garcia <wilderlizama@gmail.com>
2018-09-26 16:57:25 -0500

    componente con webpack, mejoras

--- Components-app-test/src/App.vue
+++ Components-app-test/src/App.vue
@@ -1,6 +1,7 @@
 <template>
     <section>
-        <div class="colored">{{ textillo }}</div>
+        <div class="colored" @click="mostrar">{{ textillo }}</div>
+        <div v-if="is_mostrar">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum quibusdam magnam nesciunt voluptate similique earum provident explicabo hic maxime. Culpa vel debitis qui beatae velit laboriosam, ratione obcaecati veniam?</div>
     </section>
 </template>
 
@@ -10,7 +11,13 @@
         name: "mainApp",
         data () {
             return {
-                textillo : "lorem"
+                textillo : "lorem",
+                is_mostrar : false
+            }
+        },
+        methods : {
+            mostrar () {
+                this.is_mostrar = !this.is_mostrar
             }
         }
     }
